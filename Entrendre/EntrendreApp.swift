a7b88b04706fbcdb46d43409c5df3ab4ef8f0719
FantasyHunt TecSeb marjorie0627 <hfantasy00@gmail.com sebastian.gutierrez60@gmail.com majenkinstutorials@gmail.com>
2024-02-02 11:46:14 -0500

    Initial Commit

---
+++ Entrendre/EntrendreApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntrendreApp.swift
+//  Entrendre
+//
+//  Created by Josie Marroquin on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct EntrendreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
